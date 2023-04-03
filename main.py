c51981e6a93769d5b4a30e65edc67da2c301967e
NoName <anon_2034@LAPTOP-SKAKLFVO.localdomain>
2023-04-03 06:39:32 -0400

    Maded some changes

--- main.py
+++ main.py
@@ -22,15 +22,16 @@ cursor_obj = con.cursor()
 cursor_obj.execute("SELECT * FROM weather")
 result = cursor_obj.fetchall()
 
-
+cities= []
 highTemp = []
 lowTemp = []
 
 for i in range(len(result)):
-    highTemp.append(result[i][2])
+    cities.append(result[i][0])
     lowTemp.append(result[i][1])
+    highTemp.append(result[i][2])
 
-x = np.array(["Germantown", "New York", "Dhaka", "London", "Adelaide"])
+x = np.array([cities[0], cities[1], cities[2], cities[3], cities[4]])
 y=np.array([lowTemp[0],lowTemp[1],lowTemp[2],lowTemp[3],lowTemp[4]])
 
 plt.bar(x,y)
@@ -39,7 +40,7 @@ plt.xlabel("Cities")
 plt.ylabel("Temperature")
 plt.show()
 
-a = np.array(["Germantown", "New York", "Dhaka", "London", "Adelaide"])
+a = np.array([cities[0], cities[1], cities[2], cities[3], cities[4]])
 b = np.array([highTemp[0],highTemp[1],highTemp[2],highTemp[3],highTemp[4]])
 
 plt.bar(a,b, color = "orange")
